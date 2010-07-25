d9dbaccc4a0efc7e1412c252060607041391aac1
Christopher Lee <f4milyp4nts@gmail.com>
2010-07-24 21:29:01 -0400

    Make the table clear itself between requests

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function remove_non_header_rows(table)
+{
+  for(var i = table.rows.length; i > 1; i--) {
+    table.deleteRow(i - 1);
+  }
+}
