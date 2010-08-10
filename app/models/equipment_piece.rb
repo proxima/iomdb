4bb6a0cbaad4223da9f0c513bf42558c353238c4
Christopher Lee <f4milyp4nts@gmail.com>
2010-08-09 20:34:47 -0400

    Adding a model method for cross referencing gab lists with the equipment pieces db

--- app/models/equipment_piece.rb
+++ app/models/equipment_piece.rb
@@ -15,4 +15,33 @@ class EquipmentPiece < ActiveRecord::Base
   validates_numericality_of :klass, :weight
   validates_numericality_of :tp_value, :greater_than_or_equal_to => -1, :less_than_or_equal_to => 10
 
+  def self.find_by_gab_list(gab_list)
+    rg = Regexp.new(/^ \d*    \d   ([a-zA-Z '\-()]*)([0-9,]*)\s*\w*/)
+
+    ret = {}
+
+    for line in gab_list
+      match_results = rg.match line
+      if match_results
+        item_name = match_results[1]
+        cost = match_results[2]
+
+        item_name.strip!
+        cost.delete! ','
+
+        pieces = EquipmentPiece.find(:all, :conditions => ["name = ?", "%#{item_name}%"])
+        if pieces.size == 0
+          pieces = EquipmentPiece.find(:all, :conditions => ["name LIKE ?", "%#{item_name}%"])
+        end
+
+        if pieces.size > 0
+          ret[pieces[0]] = cost
+        else
+#          puts "Couldn't find item: #{item_name}"
+        end
+      end
+    end
+    return ret
+  end
+
 end
