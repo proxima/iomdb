c70785faa6c8c44e43818ceda7f9f0e908652f1e
Christopher Lee <f4milyp4nts@gmail.com>
2010-08-11 00:47:29 -0400

    Refactor of the equipment piece index page

--- app/helpers/equipment_pieces_helper.rb
+++ app/helpers/equipment_pieces_helper.rb
@@ -1,5 +1,45 @@
 module EquipmentPiecesHelper
 
+  def armor_options
+    ret = {}
+    ret[:armors] = []
+    ret[:armors] << ['Head', 'head']
+    ret[:armors] << ['Eyes', 'eyes']
+    ret[:armors] << ['Neck', 'neck']
+    ret[:armors] << ['Cloak', 'cloak']
+    ret[:armors] << ['Amulet', 'amulet']
+    ret[:armors] << ['Torso', 'torso']
+    ret[:armors] << ['Arms', 'arms']
+    ret[:armors] << ['R. Arm', 'right arm']
+    ret[:armors] << ['L. Arm', 'left arm']
+    ret[:armors] << ['Hands', 'hands']
+    ret[:armors] << ['R. Hand', 'right hand']
+    ret[:armors] << ['L. Hand', 'left hand']
+    ret[:armors] << ['Finger', 'finger']
+    ret[:armors] << ['R. Finger', 'right finger']
+    ret[:armors] << ['L. Finger', 'left finger']
+    ret[:armors] << ['Belt', 'belt']
+    ret[:armors] << ['Legs', 'legs']
+    ret[:armors] << ['R. Leg', 'right leg']
+    ret[:armors] << ['L. Leg', 'left leg']
+    ret[:armors] << ['Feet', 'feet']
+    ret[:armors] << ['R. Foot', 'right foot']
+    ret[:armors] << ['L. Foot', 'left foot']
+    ret[:armors] << ['Multi-Slot', 'multislot']
+
+    ret[:weapons] = []
+    ret[:weapons] << ['Slashing', 'slashing']
+    ret[:weapons] << ['Piercing', 'piercing']
+    ret[:weapons] << ['Blunt', 'blunt']
+    ret[:weapons] << ['Whipping', 'whipping']
+    ret[:weapons] << ['Shield', 'shield']
+
+    ret[:admin] = []
+    ret[:admin] << ['(Unclassified)', 'unclassified']
+
+    return ret
+  end
+
   def stat_line(piece)
     ret = ""
 
