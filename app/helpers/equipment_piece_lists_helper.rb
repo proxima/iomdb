01cd20644c3f00bcdc936575e8c0164fa86f6817
Christopher Lee <f4milyp4nts@gmail.com>
2010-09-14 18:19:50 -0400

    Moving to the appropriate class

--- app/helpers/equipment_piece_lists_helper.rb
+++ app/helpers/equipment_piece_lists_helper.rb
@@ -1,2 +1,18 @@
 module EquipmentPieceListsHelper
+  def parchment_parse(parchment_text)
+    @eq_pieces = []
+    @not_found = []
+
+    parchment_text.each_line do |line|
+      temp = line.chomp
+      piece = EquipmentPiece.find_by_name(temp)
+      if piece
+        @eq_pieces << piece
+      else
+        @not_found << temp
+      end
+    end
+
+    return @eq_pieces, @not_found
+  end
 end
